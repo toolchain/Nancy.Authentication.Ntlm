83f288bbfccef80c963e8f5a6765145353ed806b
Alexey Shytikov <alexey.shytikov@gmail.com>
2012-12-26 18:09:44 +0200

    Deleting old code

--- ModuleExtensions.cs
+++ ModuleExtensions.cs
@@ -119,68 +119,6 @@ namespace Nancy.Authentication.Ntlm
                             return Unauthorized();
                         }
 
-                        //if ((module.Context.CurrentUser == null) || string.IsNullOrEmpty(module.Context.CurrentUser.UserName))
-                        //{
-                        //    string AuthorizationString = module.Request.Headers.Authorization;
-                        //    if (string.IsNullOrEmpty(AuthorizationString) || (!AuthorizationString.StartsWith("NTLM ")))
-                        //    {
-                        //        return Unauthorized();
-                        //    }
-                        //    else
-                        //    {
-                        //        byte[] token = Convert.FromBase64String(AuthorizationString.Substring(5));
-
-                        //        var state = new State();
-
-                        //        try
-                        //        {
-                        //            // First eight bytes are header containing NTLMSSP\0 signature
-                        //            // Next byte contains type of the message recieved.
-                        //            // Message Type 1 — is initial client's response to server's 401 Unauthorized error.
-                        //            // Message Type 2 — is the server's response to it. Contains random 8 bytes challenge.
-                        //            // Message Type 3 — is encrypted password hashes from client ready to server validation.
-                        //            switch (token[8])
-                        //            {
-                        //                case 1:
-                        //                    // Message of type 1 was received
-                        //                    if (EndPoint.IsServerChallengeAcquired(ref token, out state))
-                        //                    {
-                        //                        var stateId = Guid.NewGuid().ToString();
-                        //                        Sessions.Add(stateId, state);
-
-                        //                        Response response = new Response();
-                        //                        response.Cookies.Add(new NancyCookie("NTLM", stateId));
-                        //                        response.StatusCode = HttpStatusCode.Unauthorized;
-                        //                        response.Headers.Add("WWW-Authenticate", "NTLM " + Convert.ToBase64String(token));
-                        //                        return response;
-                        //                    }
-                        //                    break;
-                        //                case 3:
-                        //                    // Message of type 3 was received
-                        //                    state = Sessions[module.Request.Cookies["NTLM"]];
-
-                        //                    if (EndPoint.IsClientResponseValid(token, ref state))
-                        //                    {
-                        //                        Type3Message type3Message = new Type3Message(token);
-
-                        //                        Sessions[module.Request.Cookies["NTLM"]].ResetHandles();
-                        //                        Sessions[module.Request.Cookies["NTLM"]].UpdatePresence();
-                        //                    }
-                        //                    else
-                        //                    {
-                        //                        Sessions.Remove(module.Request.Cookies["NTLM"]);
-                        //                        return Unauthorized();
-                        //                    }
-
-                        //                    break;
-                        //            }
-                        //        }
-                        //        catch (KeyNotFoundException)
-                        //        {
-                        //            return Unauthorized();
-                        //        }
-                        //    }
-                        //}
                         return null;
                     }));
         }
