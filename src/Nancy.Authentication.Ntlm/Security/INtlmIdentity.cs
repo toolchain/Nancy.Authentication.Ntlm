9a08526842b5ac4dcdfd7c4d36fae8b5da32607b
Jonathan Merriweather <Jono@merriweather.com.au>
2013-02-21 18:31:25 +1100

    Added current user assignment

---
+++ src/Nancy.Authentication.Ntlm/Security/INtlmIdentity.cs
@@ -0,0 +1,11 @@
+﻿namespace Nancy.Authentication.Ntlm.Security
+{
+    using Nancy.Security;
+    using System;
+
+    public interface INtlmIdentity : IUserIdentity
+    {
+        string Domain { get; set; }
+        Common.NtlmFlags Flags { get; set; }
+    }
+}
